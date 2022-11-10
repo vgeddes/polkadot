0e4b1f073c70ca956dbb2f030ccbf3ff386d502c
Vsevolod Stakhov <vsevolod.stakhov@parity.io>
2022-11-10 14:13:15 +0000

    Remove accidental fmt damage

--- node/service/src/lib.rs
+++ node/service/src/lib.rs
@@ -696,9 +696,9 @@ where
 
 			// Only consider leaves that are in maximum an uncle of the best block.
 			if number < best_block.number().saturating_sub(1) {
-				return None;
+				return None
 			} else if hash == best_block.hash() {
-				return None;
+				return None
 			};
 
 			let parent_hash = client.header(&BlockId::Hash(hash)).ok()??.parent_hash;
@@ -764,9 +764,9 @@ where
 	let backoff_authoring_blocks = {
 		let mut backoff = sc_consensus_slots::BackoffAuthoringOnFinalizedHeadLagging::default();
 
-		if config.chain_spec.is_rococo()
-			|| config.chain_spec.is_wococo()
-			|| config.chain_spec.is_versi()
+		if config.chain_spec.is_rococo() ||
+			config.chain_spec.is_wococo() ||
+			config.chain_spec.is_versi()
 		{
 			// it's a testnet that's in flux, finality has stalled sometimes due
 			// to operational issues and it's annoying to slow down block
@@ -778,10 +778,10 @@ where
 	};
 
 	// If not on a known test network, warn the user that BEEFY is still experimental.
-	if enable_beefy
-		&& !config.chain_spec.is_rococo()
-		&& !config.chain_spec.is_wococo()
-		&& !config.chain_spec.is_versi()
+	if enable_beefy &&
+		!config.chain_spec.is_rococo() &&
+		!config.chain_spec.is_wococo() &&
+		!config.chain_spec.is_versi()
 	{
 		gum::warn!("BEEFY is still experimental, usage on a production network is discouraged.");
 	}
@@ -1203,10 +1203,10 @@ where
 	// Reduce grandpa load on Kusama and test networks. This will slow down finality by
 	// approximately one slot duration, but will reduce load. We would like to see the impact on
 	// Kusama, see: https://github.com/paritytech/polkadot/issues/5464
-	let gossip_duration = if chain_spec.is_versi()
-		|| chain_spec.is_wococo()
-		|| chain_spec.is_rococo()
-		|| chain_spec.is_kusama()
+	let gossip_duration = if chain_spec.is_versi() ||
+		chain_spec.is_wococo() ||
+		chain_spec.is_rococo() ||
+		chain_spec.is_kusama()
 	{
 		Duration::from_millis(2000)
 	} else {
@@ -1329,26 +1329,26 @@ pub fn new_chain_ops(
 	let telemetry_worker_handle = None;
 
 	#[cfg(feature = "rococo-native")]
-	if config.chain_spec.is_rococo()
-		|| config.chain_spec.is_wococo()
-		|| config.chain_spec.is_versi()
+	if config.chain_spec.is_rococo() ||
+		config.chain_spec.is_wococo() ||
+		config.chain_spec.is_versi()
 	{
-		return chain_ops!(config, jaeger_agent, telemetry_worker_handle; rococo_runtime, RococoExecutorDispatch, Rococo);
+		return chain_ops!(config, jaeger_agent, telemetry_worker_handle; rococo_runtime, RococoExecutorDispatch, Rococo)
 	}
 
 	#[cfg(feature = "kusama-native")]
 	if config.chain_spec.is_kusama() {
-		return chain_ops!(config, jaeger_agent, telemetry_worker_handle; kusama_runtime, KusamaExecutorDispatch, Kusama);
+		return chain_ops!(config, jaeger_agent, telemetry_worker_handle; kusama_runtime, KusamaExecutorDispatch, Kusama)
 	}
 
 	#[cfg(feature = "westend-native")]
 	if config.chain_spec.is_westend() {
-		return chain_ops!(config, jaeger_agent, telemetry_worker_handle; westend_runtime, WestendExecutorDispatch, Westend);
+		return chain_ops!(config, jaeger_agent, telemetry_worker_handle; westend_runtime, WestendExecutorDispatch, Westend)
 	}
 
 	#[cfg(feature = "polkadot-native")]
 	{
-		return chain_ops!(config, jaeger_agent, telemetry_worker_handle; polkadot_runtime, PolkadotExecutorDispatch, Polkadot);
+		return chain_ops!(config, jaeger_agent, telemetry_worker_handle; polkadot_runtime, PolkadotExecutorDispatch, Polkadot)
 	}
 	#[cfg(not(feature = "polkadot-native"))]
 	Err(Error::NoRuntime)
@@ -1377,9 +1377,9 @@ pub fn build_full(
 	hwbench: Option<sc_sysinfo::HwBench>,
 ) -> Result<NewFull<Client>, Error> {
 	#[cfg(feature = "rococo-native")]
-	if config.chain_spec.is_rococo()
-		|| config.chain_spec.is_wococo()
-		|| config.chain_spec.is_versi()
+	if config.chain_spec.is_rococo() ||
+		config.chain_spec.is_wococo() ||
+		config.chain_spec.is_versi()
 	{
 		return new_full::<rococo_runtime::RuntimeApi, RococoExecutorDispatch, _>(
 			config,
@@ -1395,7 +1395,7 @@ pub fn build_full(
 			malus_finality_delay,
 			hwbench,
 		)
-		.map(|full| full.with_client(Client::Rococo));
+		.map(|full| full.with_client(Client::Rococo))
 	}
 
 	#[cfg(feature = "kusama-native")]
@@ -1414,7 +1414,7 @@ pub fn build_full(
 			malus_finality_delay,
 			hwbench,
 		)
-		.map(|full| full.with_client(Client::Kusama));
+		.map(|full| full.with_client(Client::Kusama))
 	}
 
 	#[cfg(feature = "westend-native")]
@@ -1433,7 +1433,7 @@ pub fn build_full(
 			malus_finality_delay,
 			hwbench,
 		)
-		.map(|full| full.with_client(Client::Westend));
+		.map(|full| full.with_client(Client::Westend))
 	}
 
 	#[cfg(feature = "polkadot-native")]
@@ -1455,7 +1455,7 @@ pub fn build_full(
 			malus_finality_delay,
 			hwbench,
 		)
-		.map(|full| full.with_client(Client::Polkadot));
+		.map(|full| full.with_client(Client::Polkadot))
 	}
 
 	#[cfg(not(feature = "polkadot-native"))]
@@ -1480,7 +1480,7 @@ pub fn revert_backend(
 	let revertible = blocks.min(best_number - finalized);
 
 	if revertible == 0 {
-		return Ok(());
+		return Ok(())
 	}
 
 	let number = best_number - revertible;
